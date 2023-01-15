a29d956822c6f5b62c6894bd7cd69cd0133d959e
jordan <jordanmthomas@gmail.com>
2023-01-15 11:50:02 -0600

    connection test added

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 from pymongo import MongoClient
+
+# connect to mongodb using our URL from Atlas
+url = "mongodb+srv://admin:admin@cluster0.523bxky.mongodb.net/pytech"
+client = MongoClient(url)
+# db is the database within MongoDB we want to connect to
+db = client.pytech
+
+print("-- Pytech Collection List --")
+print(db.list_collection_names())
