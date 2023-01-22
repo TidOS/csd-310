dd8c33894b1846b40a85833b9fee05169b6d3901
jordan <jordanmthomas@gmail.com>
2023-01-21 22:26:30 -0600

    formatting update and more random name in update program 6.2

--- module_6/assignment_6.2/pytech_update.py
+++ module_6/assignment_6.2/pytech_update.py
@@ -3,14 +3,18 @@
 # Jan 18, 2023
 # CYBR-410-T301
 from pymongo import MongoClient
+from random import randrange
 
+# 6.2.3 -----------------------------------
 # connect to mongodb using our URL from Atlas
 url = "mongodb+srv://admin:admin@cluster0.523bxky.mongodb.net/pytech"
 client = MongoClient(url)
 # db is the database within MongoDB we want to connect to
 db = client.pytech
 col = db["students"]
+#------------------------------------------
 
+# 6.2.4 -----------------------------------
 # print all students in the collection
 returnedStudents = col.find({})
 print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
@@ -19,13 +23,19 @@ for i in returnedStudents:
     print("First Name: " + i["first_name"])
     print("Last Name:  " + i["last_name"])
     print()
+#------------------------------------------
 
-# update student 1007 with a new name, "someguy"
-col.update_one({"student_id": "1007"}, {"$set": {"last_name": "someguy"}})
+# 6.2.5 -----------------------------------
+# update student 1007 with a new name, "someguy" plus a random number
+col.update_one({"student_id": "1007"}, {"$set": {"last_name": "someguy" + str(randrange(1000))}})
+#------------------------------------------
 
+
+# 6.2.6 -----------------------------------
 print("-- DISPLAYING STUDENT DOCUMENT 1007 -- ")
 returnedStudent = col.find_one({"student_id": "1007"})
 print("Student ID: " + returnedStudent["student_id"])
 print("First Name: " + returnedStudent["first_name"])
 print("Last Name:  " + returnedStudent["last_name"])
 print()
+#------------------------------------------
--- module_6/assignment_6.3/pytech_delete.py
+++ module_6/assignment_6.3/pytech_delete.py
@@ -5,6 +5,7 @@
 from pymongo import MongoClient
 
 # connect to mongodb using our URL from Atlas
+# 6.3.2
 url = "mongodb+srv://admin:admin@cluster0.523bxky.mongodb.net/pytech"
 client = MongoClient(url)
 # db is the database within MongoDB we want to connect to
@@ -21,9 +22,13 @@ def printAll():
         print("Last Name:  " + i["last_name"])
         print()
 
+# 6.3.3 -----------------------------------
 # print all students using find()
 printAll()
+#------------------------------------------
 
+
+# 6.3.4 ------------------------------------
 # create a student to insert
 student1010 = {
     "student_id": "1010",
@@ -36,8 +41,9 @@ print("-- INSERT STATEMENTS --")
 returnedID = col.insert_one(student1010).inserted_id
 print("Inserted student record into the students collection with document_id " 
 + str(returnedID))
+#------------------------------------------
 
-
+# 6.3.5 -----------------------------------
 # search for the test student and print out its information
 print("-- DISPLAYING STUDENT DOCUMENT 1010 TEST DOC -- ")
 returnedStudent = col.find_one({"student_id": "1010"})
@@ -45,9 +51,14 @@ print("Student ID: " + returnedStudent["student_id"])
 print("First Name: " + returnedStudent["first_name"])
 print("Last Name:  " + returnedStudent["last_name"])
 print()
+#------------------------------------------
 
+# 6.3.6 -----------------------------------
 # delete student 1010
 col.delete_one({"student_id": "1010"})
+#------------------------------------------
 
+# 6.3.7 -----------------------------------
 # reprint all students to see if the test student was deleted
 printAll()
+#------------------------------------------
