d3680b94ff211b97d7e1923d3826ecc097100c21
jordan <jordanmthomas@gmail.com>
2023-02-23 15:48:33 -0600

    can validate customer ID

--- module_11/whatabook.py
+++ module_11/whatabook.py
@@ -62,14 +62,14 @@ def showMainMenu():
     print("[2]:  View Store Locations")
     print("[3]:  Manage Account")
     print("[4]:  Quit")
+    print( GREEN + BOLD + "__________________________" + RESETCOLOR)
 
 
 def printBooks():
     cursor.execute("SELECT * FROM book")
     books = cursor.fetchall()
-    print(RESETCOLOR)
+    print(RESETCOLOR + BOLD + INVERT + GREEN + "--    DISPLAYING BOOK LISTING    --" + RESETCOLOR)
     for book in books:
-        # print(book)
         print( GREEN + BOLD + "___________________________________________________" + RESETCOLOR)
         print(BOLD + "Book #: "  + RESETCOLOR + GREEN + str(book[0]) + RESETCOLOR)
         print(BOLD + "Book Name: " + RESETCOLOR + book[1])
@@ -80,10 +80,38 @@ def printBooks():
             print(BOLD + "Details: " + RESETCOLOR + book[3])
         except TypeError:
             print(BOLD + "Details: " + RESETCOLOR + RED + "none available" + RESETCOLOR)
-    print(INVERT + BOLD + "___________________________________________________" + RESETCOLOR)
+    print(GREEN + BOLD + "___________________________________________________\n\n" + RESETCOLOR)
 
 def printLocations():
+    print(RESETCOLOR)
+    print(BOLD + INVERT + GREEN + "--    LOCATION LISTING    --" + RESETCOLOR)
+    print( GREEN + BOLD + "___________________________________________________" + RESETCOLOR)
     cursor.execute("SELECT * FROM store")
+    stores = cursor.fetchall()
+    for store in stores:
+        print(BOLD + "Location: " + RESETCOLOR + store[1])
+        print(BOLD + "Hours: " + RESETCOLOR + str(store[2]) + BOLD + " to " + RESETCOLOR + str(store[3]))
+    print(GREEN + BOLD + "___________________________________________________\n\n" + RESETCOLOR)
+
+def printAccountMainMenu():
+    print(RESETCOLOR)
+    print(BOLD + INVERT + GREEN + "--    ACCOUNT MANAGEMENT    --" + RESETCOLOR)
+    print("[1]: Wishlist")
+    print("[2]: Add Book")   
+
+def validateUser():
+    try:
+        uid = int(input(RESETCOLOR + BOLD + "Enter your customer ID: " + RESETCOLOR))
+        cursor.execute("SELECT * FROM user WHERE user_id = " + str(uid))
+        customer = cursor.fetchone()
+        #print(customer[0])
+        return customer[0]
+    except ValueError:
+        printError("Enter a numerical customer ID.")
+        return False
+    except TypeError:
+        printError("Sorry, your customer ID was not found.")
+        return False
 
 
 try:
@@ -92,14 +120,34 @@ try:
     cursor = db.cursor()
     choice = 0
     while choice != 4:
+    # Begin main program logic
         showMainMenu()
         try:
             choice = int(input(BOLD + "ENTER CHOICE: " + RESETCOLOR + GREEN ))  
             if choice > 4 or choice < 1:
                 raise ValueError
             elif choice == 1:
+                # 1 = View Books
                 printBooks()
                 input(BOLD + "Press enter to continue: <ENTER>" + RESETCOLOR)
+            elif choice == 2:
+                # 2 = View Store Locations
+                printLocations()
+                input(BOLD + "Press enter to continue: <ENTER>" + RESETCOLOR)
+            elif choice == 3:
+                # 3 = Manage Account
+                doneUser = False
+                validUser = False
+                while not doneUser:
+                    while not validUser:
+                        customer = validateUser()
+                        if customer:
+                            validUser = True
+                    # we have a valid customer id at customer[0]
+                    doneUser = input(BOLD + "Are you done? <Y/N>: " + RESETCOLOR)
+                printAccountMainMenu()
+                
+
             print(RESETCOLOR) 
         except ValueError:
             printError("Please enter a number between 1 and 4")
